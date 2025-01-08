96ef2f1cb7bf1508304658096b015e727c110b36
vorobev.an <aleksei.suzume@gmail.com>
2025-01-08 14:42:30 +0700

    feat: frontend - Изменен язык с русского на английский & добавлены разные цвета для валидационного сообщения

--- frontend/src/app/components/body-input/body-input.component.ts
+++ frontend/src/app/components/body-input/body-input.component.ts
@@ -1,19 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-body-input',
   standalone: true,
   imports: [
     FormsModule,
-    MatButton
+    MatButton,
+    NgStyle
   ],
   templateUrl: './body-input.component.html',
   styleUrl: './body-input.component.scss'
 })
 export class BodyInputComponent {
   validationMessage: string = '';
+  isValid: boolean = true;
   @Output() bodyInputChange: EventEmitter<string> = new EventEmitter<string>();
   @Input() bodyInput: string = '';
 
@@ -32,9 +35,11 @@ export class BodyInputComponent {
     try {
       JSON.parse(this.bodyInput);
       this.validationMessage = 'Valid JSON';
+      this.isValid = true;
     } catch (error) {
       const errorMessage = (error as Error).message;
       this.validationMessage = 'Invalid JSON: ' + errorMessage;
+      this.isValid = false;
     }
   }
 }
